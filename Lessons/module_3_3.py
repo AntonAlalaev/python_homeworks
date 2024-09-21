1861f7359cdb4cd6ed8ae892ab901ed0bd794204
Anton Alalaev <antonalalaev@gmail.com>
2024-09-21 19:48:13 +0500

    Updated module_3_3.py

---
+++ Lessons/module_3_3.py
@@ -0,0 +1,14 @@
+def print_params(a=1, b='Строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(3)
+print_params(b='другая строка')
+print_params(c=10)
+values_list = [54.32, 'Привет', False]
+print_params(*values_list)  # распаковывает список и передает каждый параметр
+print_params(values_list)  # Передает только первый парамтер в качестве списка
+values_dict = {'a': 45, 'b': "Что-то", 'c': False}
+print_params(**values_dict) # Распаковывает словарь и передает каждый параметр
+print_params(values_dict) # Не распаковываеь и передает словарь в качетсве первого аргумента
