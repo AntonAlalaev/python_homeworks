e06d9e757191bc5d5a70433663422633d74913b6
Anton Alalaev <antonalalaev@gmail.com>
2024-10-04 21:15:42 +0500

    update module_6_1.py

---
+++ Lessons/module_6_1.py
@@ -0,0 +1,95 @@
+class Animal:
+    """
+    Базовый класс животного
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.alive = True
+        self.fed = False
+
+    def __str__(self):
+        return self.name
+
+
+class Plant:
+    """
+    Базовый класс растения
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.edible = False
+
+
+class Mammal(Animal):
+    """
+    Наследуемый класс млекопитающего
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def eat(self, food):
+        if isinstance(food, Plant) and food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        elif isinstance(food, Plant):
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Predator(Animal):
+    """
+    Наследуемый класс хищника
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def eat(self, food):
+        if isinstance(food, Plant) and food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        elif isinstance(food, Plant):
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+        # Решил добавить от себя, т.к. хищник может есть животных
+        if isinstance(food, Animal):
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+
+
+class Flower(Plant):
+    """
+    Наследуемый класс цветка, по умолчанию несъедобный
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Fruit(Plant):
+    """
+    Наследуемый класс фрукта, по умолчанию съедобный
+    """
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
