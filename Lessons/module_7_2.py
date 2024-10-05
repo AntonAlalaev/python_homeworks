59cf1865e78fed52e7986019ad629fb25d2e469d
Anton Alalaev <antonalalaev@gmail.com>
2024-10-05 22:26:36 +0500

    update module_7_2.py

---
+++ Lessons/module_7_2.py
@@ -0,0 +1,28 @@
+import io
+
+
+def custom_write(file_name, strings):
+    # Словарь для возврата
+    str_positions = {}
+    with open(file_name, "w", encoding="utf-8") as file:
+        # вот с этим циклом пришлось почитать что такое
+        # enumerate
+        for index, string in enumerate(strings, start=1):
+            position = file.tell()
+            file.write(string + "\n")
+            # запись в словарь
+            str_positions[(index, position)] = string
+        file.close()
+    return str_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
