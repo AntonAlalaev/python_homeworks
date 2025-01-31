c0f25769667e681e6b96da31b411f0482c9feabe
Anton Alalaev <antonalalaev@gmail.com>
2025-01-31 21:30:28 +0500

    Modified module_16_3.py

--- Lessons/module_16_3.py
+++ Lessons/module_16_3.py
@@ -7,35 +7,41 @@ users = {"1": "Имя: Example, возраст: 18"}
 
 
 @app.get("/users")
-async def get_users() -> list:
+async def get_users():
     return users
 
 
 @app.post('/user/{username}/{age}')
 async def create_user(
-        username: Annotated[str, Path(ge=5, le=20,
-                                      description='Enter username',
-                                      examples='Urban_user')],
-        age: int = Path(ge=18, le=100, discription='Enter age', examples='51')) -> dict:
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                       title="Enter username",
+                                       description="The name must be between 5 and 20 characters long",
+                                       example="Urban_user")],
+        age: Annotated[int, Path(ge=18, le=100, description='Enter age', example='46')]):
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f"Имя: {username}, возраст: {age}"
     return (f"User {current_index} is registered")
 
 
-@app.put('/user/{username}/{user_id}/{age}')
+@app.put('/user/{user_id}/{username}/{age}')
 async def update_user(
-        user_id: str = Path(ge=5, le=20,
-                            description='Enter username',
-                            examples='Urban_user'),
-        username: str = Path(ge=18, le=100,
-                             discription='Enter age',
-                             examples='51'),
-        age: int = 30) -> dict:
-    users[user_id] = user_id, username, age
-    return (f"User {user_id} has been updated")
+         user_id: Annotated[int, Path(ge=1, le=100,
+                             title="Enter user ID",
+                             description="User ID must be digit from 1 to 100",
+                             example="2")],
+         username: Annotated[str, Path(min_length=5, max_length=20,
+                            title="Enter username",
+                            description="The name must be between 5 and 20 characters long",
+                            example='Urban_user')],
+         age: Annotated[int, Path(ge=18, le=100,
+                              title='Enter age',
+                              description = "Age must be between 18 and 120",
+                              example='46')]):
+     users[user_id] = f"Имя: {username} возраст {age}"
+     return (f"User {user_id} has been updated")
 
 
 @app.delete('/user/{user_id}')
-async def delite_user(user_id: str) -> str:
-    users.pop(user_id)
-    return (f"User {user_id} has been deleted")
+async def delete_user(user_id: str) -> str:
+     users.pop(user_id)
+     return (f"User {user_id} has been deleted")
