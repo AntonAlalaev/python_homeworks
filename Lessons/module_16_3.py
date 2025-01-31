ffed71c5ffa83885a86d30233fe088294d6a0b73
Anton Alalaev <antonalalaev@gmail.com>
2025-01-31 22:01:13 +0500

    Modified module_16_3.py

--- Lessons/module_16_3.py
+++ Lessons/module_16_3.py
@@ -20,7 +20,7 @@ async def create_user(
         age: Annotated[int, Path(ge=18, le=100, description='Enter age', example='46')]):
     current_index = str(int(max(users, key=int)) + 1)
     users[current_index] = f"Имя: {username}, возраст: {age}"
-    return (f"User {current_index} is registered")
+    return f"User {current_index} is registered"
 
 
 @app.put('/user/{user_id}/{username}/{age}')
@@ -38,10 +38,10 @@ async def update_user(
                               description = "Age must be between 18 and 120",
                               example='46')]):
      users[user_id] = f"Имя: {username} возраст {age}"
-     return (f"User {user_id} has been updated")
+     return f"User {user_id} has been updated"
 
 
 @app.delete('/user/{user_id}')
 async def delete_user(user_id: str) -> str:
      users.pop(user_id)
-     return (f"User {user_id} has been deleted")
+     return f"User {user_id} has been deleted"
